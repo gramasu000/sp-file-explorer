74e4304b6e5a3f032764f6722de537c9fafcf123
Gautam <gramasu000@citymail.cuny.edu>
2019-02-24 09:40:17 -0500

    Pylinted (9/10) and commented the module

--- sp_file_explorer.py
+++ sp_file_explorer.py
@@ -1,11 +1,23 @@
+"""Module: sp_file_explorer
+
+This module defines wrapper classes for each widget in our file explorer
+It also contains a wrapper class for the entire GUI itself.
+"""
+
 from tkinter import Tk, Frame, TOP, BOTH
 
 class MainFrame:
-    
+    """MainFrame class
+
+    This class is a wrapper to the TKinter frame widget
+        that will cover the entire GUI
+    It will parent the FileListFrame and ButtonsFrame widgets
+    """
+
     def _init_settings(self):
         self.width = 800
         self.height = 600
-        self.bg = "green"
+        self.background = "green"
         self.pack_side = TOP
         self.pack_fill = BOTH
 
@@ -14,18 +26,23 @@ class MainFrame:
         self.parent = parent
         self.widget = Frame(self.parent)
         self.widget.configure(
-            background=self.bg,
+            background=self.background,
             width=self.width,
             height=self.height
         )
         self.widget.pack(
-            side=self.pack_side, 
+            side=self.pack_side,
             fill=self.pack_fill
         )
 
 
 class FileListFrame:
-    
+    """FileListFrame class
+
+    This class is a wrapper to the TKinter frame widget
+        that will hold the widgets showing the list of files in a directory.
+    """
+
     def _init_settings(self):
         self.width = 790
         self.height = 550
@@ -46,22 +63,28 @@ class FileListFrame:
             width=self.width,
             height=self.height,
             borderwidth=self.borderwidth,
-            relief=self.relief 
+            relief=self.relief
         )
         self.widget.pack(
-            side=self.pack_side, 
-            fill=self.pack_fill, 
+            side=self.pack_side,
+            fill=self.pack_fill,
             padx=self.pack_padx,
             pady=self.pack_pady
         )
 
 
 class ButtonsFrame:
-    
+    """ButtonsFrame class
+
+    This class is a wrapper to the TKinter frame widget
+        that will hold all the buttons of the app.
+    It will parent the UpButton, DownButton and CloseButton classes
+    """
+
     def _init_settings(self):
         self.width = 790
         self.height = 35
-        self.background = "orange" 
+        self.background = "orange"
         self.pack_side = TOP
         self.pack_fill = BOTH
         self.pack_padx = 5
@@ -85,10 +108,14 @@ class ButtonsFrame:
 
 
 class FileExplorerGUI:
+    """FileExplorerGUI class
+
+    This class is a wrapper to the whole GUI and all its components.
+    """
 
     def _init_settings(self):
-        self.width=800
-        self.height=600
+        self.width = 800
+        self.height = 600
         self.root.geometry(
             "{}x{}".format(self.width, self.height)
         )
@@ -103,6 +130,6 @@ class FileExplorerGUI:
 
 
 if __name__ == "__main__":
-    root = Tk()
-    gui = FileExplorerGUI(root)
-    root.mainloop()
+    ROOT = Tk()
+    GUI = FileExplorerGUI(ROOT)
+    ROOT.mainloop()
