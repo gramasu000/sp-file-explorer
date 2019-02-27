cccaeb1ae358fd1e80a50ac5184911f7238c9f15
Gautam <gramasu000@citymail.cuny.edu>
2019-02-27 11:31:41 -0500

    Added some initialization code to list out cwd and its children

--- sp_file_explorer.py
+++ sp_file_explorer.py
@@ -3,7 +3,9 @@
 This module defines wrapper classes for each widget in our file explorer
 It also contains a wrapper class for the entire GUI itself.
 """
-from tkinter import Tk
+from tkinter import Tk, StringVar
+from os import getcwd, listdir
+from os.path import dirname
 
 from gui_frames import MainFrame, FileListFrame, ButtonsFrame, ScrollListFrame
 from gui_buttons import UpButton, DownButton, CloseButton
@@ -15,7 +17,7 @@ class FileExplorerGUI:
     This class is a wrapper to the whole GUI and all its components.
     """
 
-    def _init_settings(self):
+    def _init_visual_settings(self):
         self.width = 800
         self.height = 600
         self.root.geometry(
@@ -23,9 +25,7 @@ class FileExplorerGUI:
         )
         self.root.wm_title("Simple Python File Explorer")
 
-    def __init__(self, root):
-        self.root = root
-        self._init_settings()
+    def _init_widgets(self):
         self.main_frame = MainFrame(self.root)
         self.flist_frame = FileListFrame(self.main_frame.widget)
         self.buttons_frame = ButtonsFrame(self.main_frame.widget)
@@ -35,6 +35,22 @@ class FileExplorerGUI:
         self.dir_label = DirectoryLabel(self.flist_frame.widget)
         self.scroll_list_frame = ScrollListFrame(self.flist_frame.widget)
         self.fscroll_list = FileScrollListBox(self.scroll_list_frame.widget) 
+
+    def update_dir_info(self, dir):
+        self.curdir = dir
+        self.dir_label.widget.configure(text=self.curdir)
+        self.pardir = dirname(dir)
+        self.flist = listdir(dir)
+        flist_stringvar = StringVar(self.scroll_list_frame.widget, listdir(dir))
+        self.fscroll_list.listbox.configure(listvariable=flist_stringvar)
+        if len(self.flist) is not 0:
+            self.fscroll_list.listbox.selection_set(0)
+
+    def __init__(self, root):
+        self.root = root
+        self._init_visual_settings()
+        self._init_widgets()
+        self.update_dir_info(getcwd())
 
 if __name__ == "__main__":
     ROOT = Tk()
