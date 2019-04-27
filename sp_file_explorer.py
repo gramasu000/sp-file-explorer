2ea34a3623c4babe8b972d840d7be27ebe0c76ec
Gautam <gramasu000@citymail.cuny.edu>
2019-04-27 11:40:32 -0400

    Fixed basic functionality of move up directory - a - and move down directory - z

--- sp_file_explorer.py
+++ sp_file_explorer.py
@@ -1,25 +1,48 @@
 from os import listdir, sep, getcwd
-from os.path import getmtime, getsize, isdir, isfile, islink
+from os.path import getmtime, getsize, isdir, isfile, islink, dirname
 from time import localtime
-from tkinter import Tk, Label, Listbox, Scrollbar, N, S, E, W, VERTICAL, END
+from tkinter import Tk, Label, Listbox, Scrollbar, N, S, E, W, VERTICAL, END, DISABLED, NORMAL
+
+class Reducers:
+    
+    @staticmethod
+    def moveUpDir(state):
+        newState = {}
+        newState["dir"] = dirname(state["dir"])
+        newState["children"] = listdir(newState["dir"])
+        newState["selected"] = newState["children"][0:1]
+        return newState
+
+    @staticmethod
+    def moveDownDir(state):
+        if len(state["selected"]) != 0:
+            newState = {}
+            newState["dir"] = state["dir"] + sep + state["selected"][0]
+            newState["children"] = listdir(newState["dir"])
+            newState["selected"] = newState["children"][0:1]
+            return newState
+        else:
+            return state
 
 class Application:
     
     def initUI(self, root):
         self.root = root 
         self.root.wm_title("Simple Python File Explorer")
+        self.root.geometry("800x600")
+        self.root.configure(takefocus=0)
         self.root.rowconfigure(1, weight=1)
         self.root.columnconfigure(0, weight=1)
 
-        self.label = Label(self.root, height=1, background="white")
-        self.label.grid(row=0, columnspan=2, sticky=W+E)
-        
         self.listbox = Listbox(self.root, background="white", activestyle="dotbox")
         self.listbox.grid(row=1, column=0, sticky=N+S+E+W) 
-         
-        self.scrollbar = Scrollbar(self.root, orient=VERTICAL)
+        
+        self.scrollbar = Scrollbar(self.root, orient=VERTICAL, takefocus=0)
         self.scrollbar.grid(row=1, column=1, sticky=N+S) 
         
+        self.label = Label(self.root, height=1, background="white", takefocus=0)
+        self.label.grid(row=0, columnspan=2, sticky=W+E)
+         
         self.listbox.configure(yscrollcommand=self.scrollbar.set)
         self.scrollbar["command"] = self.listbox.yview
 
@@ -33,27 +56,40 @@ class Application:
 
         # Reset Listbox of children files
         self.listbox.delete(0, END)        
-        children = self.state["children"]
-        for child in children:
+        
+        for child in self.state["children"]:
             path = dir + sep + child
-            self.listbox.insert(END, child)
-            if isdir(path):
+            if isfile(path):
+                self.listbox.insert(END, child)
                 self.listbox.itemconfig(END, background="yellow", selectbackground="orange")
+            elif isdir(path):
+                self.listbox.insert(END, child + "/") 
             elif islink(path):
+                self.listbox.insert(END, child + "/ (L)")
                 self.listbox.itemconfig(END, background="green", selectbackground="purple")
+        
+        # Select specified children
+        for child in self.state["selected"]:
+            index = self.state["children"].index(child)
+            self.listbox.selection_set(index)  
 
     def bindCallbacks(self):
-        # TODO
-        pass
-
+        render = self.render
+        moveup = Reducers.moveUpDir
+        movedown = Reducers.moveDownDir
+        self.root.bind("a", lambda event: render(moveup(self.state)))
+        self.root.bind("z", lambda event: render(movedown(self.state)))
+        self.root.bind("<<ListboxSelect>>", lambda event: render(self.state))
 
     def __init__(self, root):
-        self.state = {
+        state = {
             "dir": getcwd(),
-            "children": listdir(getcwd()) 
+            "children": listdir(getcwd()),
+            "selected": listdir(getcwd())[0:1]  
         }
         self.initUI(root)
-        self.render(self.state)
+        self.render(state)
+        self.bindCallbacks()
 
 
 if __name__ == "__main__":
