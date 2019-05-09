67cf149359d799ef502de98ea7b8ef97c6a60676
Gautam <gramasu000@citymail.cuny.edu>
2019-05-09 11:29:51 -0400

    Added much more logging and split render method into smaller functions

--- sp_file_explorer.py
+++ sp_file_explorer.py
@@ -1,3 +1,4 @@
+from sys import exit
 from os import listdir, sep, getcwd
 from os.path import getmtime, getsize, isdir, isfile, islink, dirname, join
 from time import localtime
@@ -21,6 +22,7 @@ def initLogging():
     LOGGER = logging.getLogger(__name__)
     LOGGER.addHandler(handler1)
     LOGGER.addHandler(handler2)
+    LOGGER.setLevel(logging.DEBUG)
 
 class Reducers:
 
@@ -147,97 +149,148 @@ class Reducers:
     """
 
 class Renderer:
+   
+    @staticmethod
+    def _render_label(app, state):
+        dir = state["directory"]
+        LOGGER.info(f"Rendering application - Setting Label to current directory")
+        app.label.configure(text=dir)
+        LOGGER.debug(f"Rendering application - current directory is {dir}")
     
     @staticmethod
-    def render(app, state):
-        LOGGER.info(f"Rendering application - Saving state dictionary")
-        app.state = state
-        LOGGER.debug(f"Rendering application - {app.state}")
-        
-        if state["mode"] == "quit":
-            LOGGER.info(f"Quitting Application")
-            app.root.destroy()
-            return
-        
-        LOGGER.info(f"Rendering application - Saving state dictionary")
-        app.listbox.configure(width=state["scroll_data"]["list_width"], height=state["scroll_data"]["list_size"])
-        LOGGER.debug(f"Rendering application - Listbox width is {state['scroll_data']['list_width']} characters") 
-        LOGGER.debug(f"Rendering application - Listbox height is {state['scroll_data']['list_width']} lines")
-        app.text.configure(width=state["scroll_data"]["list_width"])
-
+    def _render_listbox_items(app, state):
         dir = state["directory"]
         num_children = len(state["children"])
-        app.label.configure(text=dir)
+        LOGGER.info(f"Rendering application - Setting Listbox to contain children")
         app.listbox.delete(0, END)
+        LOGGER.debug(f"Rendering application - children list is {state['children']} of length {num_children}")
         for child in state["children"]:
             path = join(dir, child)
             if isfile(path):
+                LOGGER.debug(f"{child} is a file")
                 app.listbox.insert(END, child)
                 app.listbox.itemconfig(END, background="yellow", selectbackground="orange")
             elif isdir(path):
+                LOGGER.debug(f"{child}/ is a directory")
                 app.listbox.insert(END, child + "/") 
-        
+
+    @staticmethod
+    def _select_selected_children(app, state):
+        LOGGER.info(f"Rendering application - selecting children")
+        LOGGER.debug(f"Rendering application - selected children list is {state['selected']}")
         for child in state["selected"]:
             index = state["children"].index(child)
             app.listbox.selection_set(index)
+         
+    @staticmethod
+    def _set_scroll_position(app, state):
+        num_children = len(state["children"])
+        LOGGER.info(f"Rendering application - setting scroll")
+        fraction = state["scroll_data"]["scroll_top"] / (num_children + 1)
+        app.listbox.yview_moveto(fraction)
+        LOGGER.debug(f"Rendering application - scroll fraction is {fraction}")
             
-        app.listbox.yview_moveto(state["scroll_data"]["scroll_top"] / (num_children + 1))
-
-        #app.text.delete("1.0", END)
-        #app.text.insert(END, state["text"])                 
-        print(state["text"])
+    @staticmethod
+    def _render_text(app, state):
+        LOGGER.info(f"Rendering application - Setting text")
+        app.text.configure(state=NORMAL)
+        app.text.delete("1.0", END)
+        app.text.insert(END, state["text"])                 
+        LOGGER.debug(f"Rendering application - Text is {state['text']}")
+        LOGGER.debug(f"Rendering application - App is is {state['mode']} mode")
         if state["mode"] != "command":        
             app.text.configure(state=DISABLED)
-            # app.root.focus_set()
+            app.root.focus_set()
         else:
-            app.text.configure(state=NORMAL)
             app.text.focus_set()    
 
+    @staticmethod
+    def _save_state_in_app(app, state):
+        LOGGER.info(f"Rendering application - Saving state dictionary")
+        app.state = state
+        LOGGER.debug(f"Rendering application - State: {app.state}")
+
+    @staticmethod
+    def _set_sizes_of_listbox(app, state):
+        LOGGER.info(f"Rendering application - Sizing listbox widget")
+        app.listbox.configure(width=state["scroll_data"]["list_width"], height=state["scroll_data"]["list_size"])
+        LOGGER.debug(f"Rendering application - Listbox width is {state['scroll_data']['list_width']} characters") 
+        LOGGER.debug(f"Rendering application - Listbox height is {state['scroll_data']['list_size']} lines")
+
+    @staticmethod
+    def _check_quit_mode(app, state):
+        if state["mode"] == "quit":
+            LOGGER.info(f"Quitting Application")
+            app.root.destroy()
+            exit(0)
+
+    @classmethod
+    def render(cls, app, state):
+        LOGGER.info(f"Rendering application")
+        cls._check_quit_mode(app, state)
+        cls._save_state_in_app(app, state)
+        cls._set_sizes_of_listbox(app, state)
+        cls._render_label(app, state)
+        cls._render_listbox_items(app, state)
+        cls._select_selected_children(app, state)
+        cls._set_scroll_position(app, state)
+        cls._render_text(app, state)
+        
+        
 class Application:
     
-    def initUI(self, root):
-        self.root = root 
-        self.root.wm_title("Simple Python File Explorer")
-        self.root.rowconfigure(1, weight=1)
-        self.root.columnconfigure(0, weight=1)
+    def initUI(app, root):
+        LOGGER.info("Initializing User Interface")
+        app.root = root 
+        app.root.wm_title("Simple Python File Explorer")
+        app.root.rowconfigure(1, weight=1)
+        app.root.columnconfigure(0, weight=1)
 
-        self.listbox = Listbox(self.root, background="white", activestyle="dotbox", takefocus=0)
-        self.listbox.grid(row=1, column=0, sticky=N+S+E+W) 
+        LOGGER.debug("Initializing User Interface - Creating scrollable listbox which expands horizontally and vertically with root window")
+        app.listbox = Listbox(app.root, background="white", activestyle="dotbox", takefocus=0)
+        app.listbox.grid(row=1, column=0, sticky=N+S+E+W) 
         
-        self.scrollbar = Scrollbar(self.root, orient=VERTICAL, takefocus=0)
-        self.scrollbar.grid(row=1, column=1, sticky=N+S) 
+        app.scrollbar = Scrollbar(app.root, orient=VERTICAL, takefocus=0)
+        app.scrollbar.grid(row=1, column=1, sticky=N+S) 
         
-        self.label = Label(self.root, height=1, background="white", takefocus=0)
-        self.label.grid(row=0, columnspan=2, sticky=W+E)
+        app.listbox.configure(yscrollcommand=app.scrollbar.set)
+        #app.scrollbar["command"] = app.listbox.yview
+        
+        LOGGER.debug("Initializing User Interface - Creating top label which expands horizontally with root window")
+        app.label = Label(app.root, height=1, background="white", takefocus=0)
+        app.label.grid(row=0, columnspan=2, sticky=W+E)
          
-        self.text = Text(self.root, height=1, background="white", wrap=NONE, takefocus=0)
-        self.text.grid(row=2, columnspan=2, sticky=W+E)
+        LOGGER.debug("Initializing User Interface - Creating top text which expands horizontally with root window")
+        app.text = Text(app.root, height=1, background="white", wrap=NONE, takefocus=0)
+        app.text.grid(row=2, columnspan=2, sticky=W+E)
 
-        self.listbox.configure(yscrollcommand=self.scrollbar.set)
-        #self.scrollbar["command"] = self.listbox.yview
 
-    def bindCallbacks(self):
-        pass
-        #self.root.bind("-", lambda event: self.render(Reducers.moveUpDir(self.state)))
-        #self.root.bind("<Return>", lambda event: self.render(Reducers.moveDownDir(self.state)))
-        self.root.bind("<<ListboxSelect>>", lambda event: Renderer.render(self, Reducers.changeModeToBrowse(self.state)))
-        self.root.bind("l", lambda event: Renderer.render(self, Reducers.changeModeToBrowse(self.state)))
-        #self.root.bind("<Down>", lambda event: self.render(Reducers.moveDownSelection(self.state)))
-        #self.listbox.bind("<Up>", lambda event: self.render(Reducers.moveTopSelection(self.state)))
-        #self.root.bind("<Up>", lambda event: self.render(Reducers.moveUpSelection(self.state)))
-        #self.root.bind("g", lambda event: self.render(Reducers.moveTopSelection(self.state)))
-        #self.root.bind("<Shift-KeyPress-G>", lambda event: self.render(Reducers.moveBottomSelection(self.state)))
-        #self.root.bind("q", lambda event: self.render(Reducers.quit(self.state)))
+    def bindCallbacks(app):
+        LOGGER.info(f"Binding Callbacks")
+        #app.root.bind("-", lambda event: app.render(Reducers.moveUpDir(app.state)))
+        #app.root.bind("<Return>", lambda event: app.render(Reducers.moveDownDir(app.state)))
+        LOGGER.debug(f"Binding virtual event of listbox selection with mouse to changeModeToBrowse reducer - so the mouse does not affect selection")
+        app.root.bind("<<ListboxSelect>>", lambda event: Renderer.render(app, Reducers.changeModeToBrowse(app.state)))
+        LOGGER.debug(f"Binding 'l' key to changeModeToCommand reducer")
+        app.root.bind("l", lambda event: Renderer.render(app, Reducers.changeModeToCommand(app.state)))
+        #app.root.bind("<Down>", lambda event: app.render(Reducers.moveDownSelection(app.state)))
+        #app.listbox.bind("<Up>", lambda event: app.render(Reducers.moveTopSelection(app.state)))
+        #app.root.bind("<Up>", lambda event: app.render(Reducers.moveUpSelection(app.state)))
+        #app.root.bind("g", lambda event: app.render(Reducers.moveTopSelection(app.state)))
+        #app.root.bind("<Shift-KeyPress-G>", lambda event: app.render(Reducers.moveBottomSelection(app.state)))
+        #app.root.bind("q", lambda event: app.render(Reducers.quit(app.state)))
 
-    def __init__(self, root):
-        self.state = Reducers.getInitState()
-        self.initUI(root)
-        Renderer.render(self, self.state)
-        self.bindCallbacks()
+    def __init__(app, root):
+        app.state = Reducers.getInitState()
+        app.initUI(root)
+        Renderer.render(app, app.state)
+        app.bindCallbacks()
 
 
 if __name__ == "__main__":
     initLogging()
+    print(LOGGER)
+    LOGGER.info("Starting Application")
     ROOT = Tk()
     APP = Application(ROOT)
     APP.root.mainloop()
